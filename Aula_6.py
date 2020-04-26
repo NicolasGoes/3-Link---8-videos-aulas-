1a01fba0503a917dad5d017a8faaeb60f7ad91f6
NicolasGoes <60896996+NicolasGoes@users.noreply.github.com>
2020-04-26 03:15:15 -0300

    Add files via upload

---
+++ Aula_6.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+variavel = tf.Variable(2)
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    rodar = sess.run(variavel)
+    #print(rodar)
+
+    matriz = tf.random_normal((3,5),0,1)
+    variavel2 = tf.Variable(matriz)
+    #print(matriz)
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    rodar = sess.run(variavel2)
+    print(rodar)
